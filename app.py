93ab35e5b03ad86d3ef923e7c9c2bf54104cd041
arzubekm <arzubekm@gmail.com>
2021-05-04 17:05:11 +0900

    Working on jquery autocomplete on form.html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, render_template
 import recommendation
+import pandas as pd
 
 app = Flask(__name__)
 
+def get_data():
+    data = pd.read_csv('half_players_21_clean.csv')
+    return data
+
 @app.route('/')
 def homepage():
-    return render_template('form.html')
+    data = get_data()
+    long_names = data.long_name
+    # long_names = ["Lionel Messi", "Cristiano Ronaldo dos Santos Aveiro", "Jan Oblak", "Robert Lewandowski", "Kevin De Bruyne"]
+
+    return render_template('temp.html', long_names=long_names)
 
 @app.route('/player', methods=['GET', 'POST'])
 def recommend_players():
